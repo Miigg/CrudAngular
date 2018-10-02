2c30e106d72b7a4fd0860b53dda86dab07cd2b5a
Miguel Nogueira <miguel.nogueira@outlook.com>
2018-10-02 15:19:43 -0300

    Add project files.

---
+++ CrudAngular/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace ASPCoreWithAngular.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Gender { get; set; }
+
+        public string Department { get; set; }
+
+        public string City { get; set; }
+    }
+}
